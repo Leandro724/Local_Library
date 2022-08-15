0dc58d320dd83308b59b8d9d9a3a251cd7fe54a3
Leandro724 <88081194+Leandro724@users.noreply.github.com>
2022-08-15 12:14:43 +0200

    Index page layout
    Created the frontend of the homepage

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+      <!-- Linking in the stylesheet -->
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <!-- Login form for each user -->
+    <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
+		<h1>Librarian Login</h1>
+		
+		<label>Email</label>
+		<input class="inputField" type="text" name="Email" required>
+
+		<label>Password</label>
+		<input class="inputField" type="text" name="Password" required>
+
+		<button class="button" type="submit" name="submit">Log in</button>
+
+		<p class="error"><?php echo @$user->error ?></p>
+		<p class="success"><?php echo @$user->success ?></p>
+		<p>Don't have an account? <a href="SignUp.php">Register Here.</a></p><br><br>
+	</form>
+
+    <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
+		<h1>Members Login</h1>
+		
+		<label>Email</label>
+		<input class="inputField" type="text" name="Email" required>
+
+		<label>Password</label>
+		<input class="inputField" type="text" name="Password" required>
+
+		<button class="button" type="submit" name="submit">Log in</button>
+
+		<p class="error"><?php echo @$user->error ?></p>
+		<p class="success"><?php echo @$user->success ?></p>
+		<p>Forgot password? <a href="SignUp.php">Click Here.</a></p><br><br>
+	</form>
+   
+</body>
+</html>
